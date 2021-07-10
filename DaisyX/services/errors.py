9cde2155da178d9f3408fb0be2dfcf2df78cc5b4
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-10 18:42:01 +0530

    Small Update

--- DaisyX/services/errors.py
+++ DaisyX/services/errors.py
@@ -30,6 +30,9 @@ def capture_err(func):
     async def capture(client, message, *args, **kwargs):
         try:
             return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
@@ -39,14 +42,16 @@ def capture_err(func):
             )
             error_feedback = split_limits(
                 "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
-                    0 if not message.from_user else message.from_user.id,
+                    0
+                    if not message.from_user
+                    else message.from_user.id,
                     0 if not message.chat else message.chat.id,
                     message.text or message.caption,
                     "".join(errors),
                 ),
             )
             for x in error_feedback:
-                await pbot.send_message(SUPPORT_CHAT, x)
+                await app.send_message(LOG_GROUP_ID, x)
             raise err
 
     return capture

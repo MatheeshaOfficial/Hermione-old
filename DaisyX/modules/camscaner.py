e4f2827f74bfcc19c4dc0edfce3bb5910eea45b7
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-11 18:50:40 +0530

    Update camscaner.py

--- DaisyX/modules/camscaner.py
+++ DaisyX/modules/camscaner.py
@@ -77,5 +77,14 @@ async def asciiart(event):
 __mod_name__ = "Hi"
 __help__ = """
 <b>CamScaner</b>
-- Nothing Here 🥺
+ - /camscanner: Reply to a image to scan and improve it's clarity.
+**Instructions**
+▪️The image should be a page with some written text on it (screenshots aren't permitted)
+▪️The image should contain the page with four corners clearly visible
+▪️The background should be somewhat darker than the page
+▪️The image should contain only the page with no other objects like pencil, eraser etc. beside it(within the image)
+**PRO TIP**
+You can simply draw a border(a black square) around the portion you want to scan for better efficiency and edge detection
+If you are still messed up send `/helpcamscanner` in pm for the tutorial
+𝑷𝒐𝒘𝒆𝒓𝒅 𝑩𝒚 @HermioneUpdates
 """

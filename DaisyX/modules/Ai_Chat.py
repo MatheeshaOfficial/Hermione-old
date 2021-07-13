f9ef0a1133a0b953a947341ae8cb9e9a44c6af24
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-13 18:44:05 +0530

    Good
    I get my own chatbot

--- DaisyX/modules/Ai_Chat.py
+++ DaisyX/modules/Ai_Chat.py
@@ -337,3 +337,19 @@ async def inuka(client, message):
         await message.reply_text(pro)
     except CFError as e:
         print(e)
+        
+
+__help__ = """
+<b> Chatbot </b>
+DAISY AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+ - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
+ - /chatbot EN : Enables English only chatbot
+ 
+ 
+<b> Assistant </b>
+ - /ask [question]: Ask question from daisy
+ - /ask [reply to voice note]: Get voice reply
+ 
+"""
+
+__mod_name__ = "AI Assistant"

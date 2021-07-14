208c11cc17a10590d6bda7f8b7534577813f32a6
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-14 12:29:56 +0530

    Create states.py

---
+++ DaisyX/modules/states.py
@@ -0,0 +1,16 @@
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - bot_start_time)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    stats = f"""
+{USERBOT_USERNAME}@William
+------------------
+UPTIME: {formatter.get_readable_time((bot_uptime))}
+BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+"""
+    return stats

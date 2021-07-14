b2379c978c94630103348dedc337a64319464d08
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-14 18:59:15 +0530

    Update states.py

--- DaisyX/modules/states.py
+++ DaisyX/modules/states.py
@@ -1,12 +1,16 @@
 # Stats Module
-
+import asyncio
 import os
+import subprocess
 import time
 
 import psutil
-from wbb import bot_start_time
-from wbb.utils import formatter
+from pyrogram import filters
+from pyrogram.errors import FloodWait
 
+from wbb import (BOT_ID, bot_start_time)
+from wbb.core.decorators.errors import capture_err
+from wbb.utils import formatter
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - bot_start_time)
@@ -24,3 +28,5 @@ RAM: {mem}%
 DISK: {disk}%
 """
     return stats
+
+

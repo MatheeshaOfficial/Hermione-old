fe71d28faf7ea43e5caf8286fa9627c21a39ce50
InukaAsith <inukaasith7@gmail.com>
2021-07-14 13:38:40 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/states.py
+++ DaisyX/modules/states.py
@@ -1,18 +1,13 @@
-
-
 # Stats Module
-import asyncio
 import os
-import subprocess
 import time
 
 import psutil
 from pyrogram import filters
-from pyrogram.errors import FloodWait
-
-from wbb import (BOT_ID, bot_start_time)
+from wbb import bot_start_time
 from wbb.core.decorators.errors import capture_err
 from wbb.utils import formatter
+
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - bot_start_time)

6baa085a5b38ede6b464e8cbd0bb66ec2ddbe93a
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-18 14:44:29 +0530

    Update GroupGardian.py

--- DaisyX/modules/extra/GroupGardian.py
+++ DaisyX/modules/extra/GroupGardian.py
@@ -151,7 +151,7 @@ async def ws(event):
         await event.delete()
         st = sender.first_name
         hh = sender.id
-        final = f"**NSFW DETECTED**\n\n[{st}](tg://user?id={hh}) your message contain NSFW content.. So, Daisy deleted the message\n\n **Nsfw Sender - User / Bot :** [{st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By DaisyAI` \n**#GROUP_GUARDIAN** "
+        final = f"**NSFW DETECTED**\n\n[{st}](tg://user?id={hh}) your message contain NSFW content.. So, Hermione deleted the message\n\n **Nsfw Sender - User / Bot :** [{st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By DaisyAI` \n**#GROUP_GUARDIAN** "
         dev = await event.respond(final)
         await asyncio.sleep(30)
         await dev.delete()
@@ -440,7 +440,7 @@ async def del_profanity(event):
  
 __help__ = """
 <b> Group Guardian: </b>
-✪ Daisy can protect your group from NSFW senders, Slag word users and also can force members to use English
+✪ Hermione can protect your group from NSFW senders, Slag word users and also can force members to use English
  
 <b>Commmands</b>
  - /nsfwguardian <i>on/off</i> - Enable|Disable Porn cleaning

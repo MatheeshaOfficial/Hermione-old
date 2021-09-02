c5cf1bf4f6d624d2af118eede391a68b7a86c860
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-09-02 23:31:16 +0530

    Update caption_editor.py

--- DaisyX/modules/caption_editor.py
+++ DaisyX/modules/caption_editor.py
@@ -1,8 +1,4 @@
-# Copyright (C) 2021 TeamDaisyX
-
-
 # This file is part of Daisy (Telegram Bot)
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
